dd4c35979c93dfe5f6dd5d24fa3523c005a65ff4
zzZ5 <baoju_liu@foxmail.com>
2019-08-12 16:07:11 +0800

    添加setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setup(
+    name='html2epub',
+    version='1.0',
+    author='zzZ5',
+    author_email='baoju_liu@foxmail.com',
+    description='将 html链接, html文件 或 html文本 转换成 epub文件.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/zzZ5/Html2Epub',
+    packages=['html2epub'],
+    package_data={'html2epub': ['epub_templates/*', ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'beautifulsoup4',
+        'jinja2',
+        'requests',
+    ]
+)
