6e4b7915a78af92e95df799f8314ad60b14556df
zzZ5 <baoju_liu@foxmail.com>
2019-08-20 18:44:00 +0800

    更新了线上版本

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name='html2epub',
-    version='1.0',
+    version='1.01',
     author='zzZ5',
     author_email='baoju_liu@foxmail.com',
     description='将 html链接, html文件 或 html文本 转换成 epub文件.',
